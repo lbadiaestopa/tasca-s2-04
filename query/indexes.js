28401a229061741b22055c0e39a17d513c02a477
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-19 11:30:51 +0100

    Add additional indexes for restaurant queries

--- query/indexes.js
+++ query/indexes.js
@@ -1,3 +1,6 @@
 //Use this file to define indexes for your queries
 db.restaurants.createIndex({ name: 1 });
+db.restaurants.createIndex({ borough: 1 });
+db.restaurants.createIndex({ cuisine: 1 });
 db.restaurants.createIndex({ location: "2dsphere" });
+db.restaurants.createIndex({ "grades.score": 1 });
